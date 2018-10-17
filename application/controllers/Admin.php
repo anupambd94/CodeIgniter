e1973eb3070dedef777320f0981c90fcae3f7b7b
Anupam Hayat <anupam.bd94@gmail.com>
2018-10-18 05:14:25 +0600

    added admin login form and modified few things

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,14 +1,13 @@
 <?php
 class Admin extends MY_Controller{
   public function index(){
-    $this->load->library('form_validation');
     $query = $this->db->get('users');
     $this->form_validation->set_rules('uname','User Name','required|is_unique[users.username]|alpha_numeric');
     $this->form_validation->set_rules('pass','Password','required|max_length[12]|min_length[6]');
     if($this->form_validation->run()){
       echo "validation Successfull";
     }else{
-      $this->load->view('Users/article_list');
+      $this->load->view('Admin/login');
       //echo validation_errors();
     }
   }
---
+++ application/views/Admin/login.php
@@ -0,0 +1,63 @@
+
+<?php include('header.php');?>
+
+<div class="container" style="margin-top:50px;">
+
+  <?php echo form_open('admin/index'); ?>
+    <fieldset>
+      <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-8">
+          <h1>Login Form</h1>
+
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="email">Username</label>
+
+                <?php echo form_input(['class'=>'form-control','name'=>'uname','placeholder'=>'Enter username','id'=>'username','type'=>'text','value'=>set_value('uname')]); ?>
+              </div>
+
+            </div>
+            <div class="col-lg-6" style="margin-top:35px;">
+              <?php echo form_error('uname',"<div class='text-danger'>","</div>");?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="password">Password</label>
+                <?php echo form_password(['class'=>'form-control','name'=>'pass','placeholder'=>'Enter password','id'=>'password','value'=>set_value('pass')]); ?>
+              </div>
+
+            </div>
+            <div class="col-lg-6" style="margin-top:35px;">
+              <?php echo form_error('pass',"<div class='text-danger'>","</div>");?>
+            </div>
+
+          </div>
+
+
+          <fieldset class="form-group">
+            <div class="form-check">
+              <label class="form-check-label">
+                <?php echo form_checkbox(['class'=>'form-check-input','type'=>'checkbox']); ?>
+                Remember me
+              </label>
+            </div>
+          </fieldset>
+          <?php echo form_submit(['class'=>'btn btn-primary','type'=>'submit','value'=>'Login']); ?>
+          <?php echo form_reset(['class'=>'btn btn-danger','type'=>'reset','value'=>'Reset']); ?>
+
+        </div>
+
+      </div>
+
+
+    </fieldset>
+  </form>
+</div>
+
+
+ <?php include('footer.php');?>
